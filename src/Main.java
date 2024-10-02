7352d4dc63d7029b5641122c56b454fbd5381d60
Aikel <aikelen.030904@gmail.com>
2024-10-03 01:57:21 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,110 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("helo world");
+        //1
+        int[] weight = new int[3];
+        weight[0] = 1;
+        weight[1] = 2;
+        weight[2] = 3;
+
+        double[] fractionalNumbers = {1.57, 7.654, 9.986};
+
+        String[] randomArray = {"First", "Second", "Third"};
+
+        printArrayInt(weight);
+        printArrayDouble(fractionalNumbers);
+        printArrayString(randomArray);
+
+        printArrayIntReserve(weight);
+        printArrayDoubleResrve(fractionalNumbers);
+        printArrayStringReserve(randomArray);
+
+        printArrayEventNumbers(weight);
+    }
+    //2
+    public static void printArrayInt(int[] array){
+        for(int i = 0; i < array.length;i++) {
+            if (i == (array.length - 1)) {
+                System.out.print(array[i]);
+            } else {
+                System.out.print(array[i] + ", ");
+            }
+        }
+        System.out.println();
+    }
+    public static void printArrayDouble(double[] array){
+        for(int i = 0; i < array.length;i++) {
+            if (i == (array.length - 1)) {
+                System.out.print(array[i]);
+            } else {
+                System.out.print(array[i] + ", ");
+            }
+        }
+        System.out.println();
+    }
+    public static void printArrayString(String[] array){
+        for(int i = 0; i < array.length;i++) {
+            if (i == (array.length - 1)) {
+                System.out.print(array[i]);
+            } else {
+                System.out.print(array[i] + ", ");
+            }
+        }
+        System.out.println();
+        System.out.println();
+    }
+    //3
+
+    public static void printArrayIntReserve(int[] array){
+        for (int i = array.length - 1; i>=0;i--){
+            if(i==0){
+                System.out.print(array[i]);
+            } else {
+                System.out.print(array[i]+", ");
+            }
+        }
+        System.out.println();
+    }
+    public static void printArrayDoubleResrve(double[] array){
+        for (int i = array.length - 1; i>=0;i--){
+            if(i==0){
+                System.out.print(array[i]);
+            } else {
+                System.out.print(array[i]+", ");
+            }
+        }
+        System.out.println();
+    }
+    public static void printArrayStringReserve(String[] array){
+        for (int i = array.length - 1; i>=0;i--){
+            if (i==0){
+                System.out.print(array[i]);
+
+            } else {
+                System.out.print(array[i]+", ");
+            }
+        }
+        System.out.println();
+        System.out.println();
+    }
+
+    //4
+    public static void printArrayEventNumbers(int[] array){
+        for (int i = 0; i < array.length;i++){
+            if (i == array.length-1){
+                if (array[i] % 2 != 0){
+                    array[i]+=1;
+                    System.out.print(array[i]);
+
+                }
+            } else {
+                if (array[i] % 2 != 0) {
+                    array[i]+=1;
+                    System.out.print(array[i]+", ");
+                }else {
+                    System.out.print(array[i]+", ");
+                }
+            }
+        }
     }
 
 }
